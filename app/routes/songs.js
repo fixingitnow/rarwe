299098b5d017e6da8c6c2fa20f9e032b6763a109
fixingitnow <pradeep@envoy.com>
2024-06-11 11:02:16 -0700

    add nested routing

--- app/routes/songs.js
+++ app/routes/songs.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default class SongsRoute extends Route {
   model() {
     let band = this.modelFor('bands.band');
-    console.log(band, 'band name');
+
     return band.songs;
   }
 }
